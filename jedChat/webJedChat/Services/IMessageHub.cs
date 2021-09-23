400d80a39aa350c79840620a489715f7608c2480
Jason Dickmeyer <jdickmeyer@nationalchecking.com>
2021-09-23 17:44:19 -0500

    initial commit

---
+++ jedChat/webJedChat/Services/IMessageHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jedChat.Services
+{
+    public interface IMessageHub
+    {
+        Task BroadcastMessage(string user, string message);
+        Task ReceiveMessage();
+    }
+}
---
+++ jedChat/webJedChat/Services/MessageHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jedChat.Services
+{
+    public class MessageHub : Hub, IMessageHub
+    {
+        public MessageHub()
+        {
+
+        }
+
+        public Task BroadcastMessage(string user, string message) {
+
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        
+        
+        }
+        public Task ReceiveMessage() { return; }
+    }
+}
